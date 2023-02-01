92ee5bec698450b346a680f4d4da0be2739a3dfe
DeTovelli <detovelli@gmail.com>
2023-02-01 10:06:24 +0300

    task 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
   
-}
-
-function getUsersNamesInAgeRange(users, gender) {
+    if (arr1.length === arr2.length && arr1.every((element, i) => element === arr2[i])) {
+      return true;
+    } else {
+      return false;
+    }
   
-}
+  }
+  
+  function advancedFilter(arr) {
+  
+    return arr
+    .filter((numbers) => numbers > 0)
+    .filter((numbers) => numbers % 3 === 0)
+    .map((numbers) => numbers * 10);
+  }
